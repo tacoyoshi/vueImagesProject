7a427bac6b953c76579bb076bdef5d6f28e873b7
Jeff Easton <coltyg09@gmail.com>
2020-02-20 18:58:19 -0800

    FInished images project

--- src/api/imgur.js
+++ src/api/imgur.js
@@ -1,4 +1,5 @@
 import qs from 'qs';
+import axios from 'axios';
 
 //const CLIENT_ID = process.env.VUE_APP_CLIENT_ID;
 const ROOT_URL = 'https://api.imgur.com';
@@ -9,6 +10,29 @@ export default {
       client_id: process.env.VUE_APP_CLIENT_ID,
       response_type: 'token'
     };
+
     window.location = `${ROOT_URL}//oauth2/authorize?${qs.stringify(querystring)}`;
+  },
+
+  fetchImages(token) {
+    return axios.get(`${ROOT_URL}/3/account/me/images`, {
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    })
+  },
+
+  uploadImages(images, token) {
+      const promises = Array.from(images).map(image => {
+        const formData = new FormData();
+        formData.append('image', image);
+        return axios.post(`${ROOT_URL}/3/image`, formData, {
+          headers: {
+            Authorization: `Bearer ${token}`
+          }
+        });
+      });
+      return Promise.all(promises);
   }
+
 };
--- src/components/ImageList.vue
+++ src/components/ImageList.vue
@@ -1,14 +1,37 @@
 <template>
   <div>
-    Image List
+    <div v-if="isLoggedIn" class="image-container">
+      <img v-for="image in allImages"
+      :key='image.id'
+      :src="image.link"/>
+    </div>
+    <div v-else>
+      <h2>Log in to get started!</h2>
+    </div>
   </div>
 </template>
 
 <script>
+  import { mapActions, mapGetters } from 'vuex';
   export default {
-    name: 'ImageList'
-  }
+    name: 'ImageList',
+    computed: mapGetters([ 'allImages', 'isLoggedIn' ]),
+    methods: mapActions(['fetchImages']),
+    created() {
+      this.fetchImages();
+    }
+  };
 </script>
 
 <style scoped>
+  .image-container {
+    column-count: 3;
+    column-gap: 0;
+  }
+
+  img {
+    max-width: 100%;
+    padding: 5px;
+  }
+
 </style>
